a76e065c4aaec0f8cb7d32d2011b5de3055d1c31
Dawn Saquin <dawnsaq@gmail.com>
2019-02-28 17:09:34 -0800

    testing on seeding

--- seeds/generateCSVs.js
+++ seeds/generateCSVs.js
@@ -1,4 +1,4 @@
-const { generateFakeProduct, generateFakeVariants } = require('../db/fakerCreators.js');
+const { generateFakeProduct, generateFakeVariants } = require('../db/fakerCreators');
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
@@ -6,25 +6,26 @@ const util = require('util');
 const writeFile = util.promisify(fs.writeFile);
 const readFile = util.promisify(fs.readFile);
 
-// const handleProductCSV = () => {
-//   for (let i = 1; i < 11; i++) {
-//     var sampleProducts = [];
-//     for (let j = 0; j < 100; j++) {
-//       sampleProducts.push(generateFakeProduct());
-//     }
+const handleProductCSV = () => {
+  for (let i = 1; i < 11; i++) {
+    var sampleProducts = [];
+    for (let j = 0; j < 100; j++) {
+      sampleProducts.push(generateFakeProduct());
+    }
 
-//     const productInJSONFormat = JSON.stringify(sampleProducts);
-//     const pathway = path.join(__dirname, `/productData/productData${i}.csv`);
-//     writeFile(pathway, productInJSONFormat)
-//       .then(() => console.log('file created successfully'))
-//       .then(() => {
-//         readFile(pathway, 'utf-8')
-//           .then(data => console.log(data))
-//           .catch(err => console.log('Error', err));
-//       });
-//   }
-// };
-// handleProductCSV();
+    const productInJSONFormat = JSON.stringify(sampleProducts);
+    const pathway = path.join(__dirname, `/productData/productData${i}.csv`);
+    return writeFile(pathway, productInJSONFormat)
+      .then(() => console.log('file created successfully'))
+      .then(() => {
+        readFile(pathway, 'utf-8')
+          .then(data => {
+            return data;
+          })
+          .catch(err => console.log('Error', err));
+      });
+  }
+};
 
 const handleVariantCSV = () => {
   for (let i = 1; i < 31; i++) {
@@ -36,86 +37,21 @@ const handleVariantCSV = () => {
         variants.push(variantList);
       }
     }
-    console.log('final variant list');
     var flattenVariantList = [].concat.apply([], variants);
     const productInJSONFormat = JSON.stringify(flattenVariantList);
 
     const pathway = path.join(__dirname, `/variantData/variantData${i}.csv`);
-    writeFile(pathway, productInJSONFormat)
+    return writeFile(pathway, productInJSONFormat)
       .then(() => console.log('file created successfully'))
       .then(() => {
         readFile(pathway, 'utf-8')
-          .then(data => console.log(data))
+          .then(data => {
+            return data;
+          })
           .catch(err => console.log('Error', err));
       });
   }
 };
-handleVariantCSV();
 
-///////
-
-//         const getFileContent = () => {
-//           const pathway = path.join(__dirname, `/productData/productData${i}.csv`);
-//           console.log('right here i am');
-//           console.log(i);
-//           fs.readFile(pathway, (err, fileData) => {
-//             if (err) {
-//               throw 'error writing product data into file';
-//             }
-//             console.log('here');
-//             files = JSON.parse(fileData);
-//             knex('products').insert(files);
-//           });
-//         };
-//         // for (let n = 1; n < 11; n++) {
-
-//         // return;
-//         // }
-//         writeFileContent(sampleProducts, getFileContent);
-//       }
-//       return;
-
-//       // return knex('products').insert(sampleProducts);
-//     })
-//     .then(() => {
-//       return knex('products')
-//         .pluck('itemId')
-//         .then(itemIds => {
-//           console.log('here are the product ids---');
-//           console.log(itemIds);
-//           for (let i = 1; i < 31; i++) {
-//             var variants = [];
-//             for (let i = 0; i < itemIds.length; i++) {
-//               let itemId = itemIds[i];
-//               let variantList = generateFakeVariants(itemId);
-//               variants.push(variantList);
-//             }
-//             console.log('final variant list');
-//             console.log(variants);
-//             var flattenVariantList = [].concat.apply([], variants);
-//             const productInJSONFormat = JSON.stringify(flattenVariantList);
-//             const pathway = path.join(__dirname, `/variantData/variantData${i}.csv`);
-//             fs.writeFile(pathway, productInJSONFormat, err => {
-//               if (err) {
-//                 throw 'error writing product data into file';
-//               } else {
-//                 console.log('successfully wrote variant csv file');
-//               }
-//             });
-//           }
-
-//           for (let n = 1; n < 31; n++) {
-//             const pathway = path.join(__dirname, `/variantData/variantData${n}.csv`);
-//             fs.readFile(pathway, (err, fileData) => {
-//               if (err) {
-//                 throw 'error writing product data into file';
-//               } else {
-//                 files = JSON.parse(fileData);
-//                 knex('variants').insert(files);
-//               }
-//             });
-//           }
-//           return;
-//         });
-//     });
-// };
+exports.handleProductCSV = handleProductCSV;
+exports.handleVariantCSV = handleVariantCSV;
--- seeds/products.js
+++ seeds/products.js
@@ -1,4 +1,5 @@
-const { generateFakeProduct, generateFakeVariants } = require('../db/fakerCreators.js');
+const { handleProductCSV, handleVariantCSV } = require('./generateCSVs');
+const {} = require('./generateCSVs');
 const fs = require('fs');
 const path = require('path');
 
@@ -9,89 +10,14 @@ exports.seed = (knex, Promise) => {
       knex('products').del();
     })
     .then(() => {
-      for (let i = 1; i < 11; i++) {
-        var sampleProducts = [];
-        for (let j = 0; j < 100; j++) {
-          sampleProducts.push(generateFakeProduct());
-        }
-
-        const writeFileContent = (products, callback) => {
-          const productInJSONFormat = JSON.stringify(products);
-          const pathway = path.join(__dirname, `/productData/productData${i}.csv`);
-          fs.writeFile(pathway, productInJSONFormat, err => {
-            if (err) {
-              throw 'error writing product data into file';
-            } else {
-              console.log('successfully wrote product csv file');
-              callback();
-            }
-          });
-        };
-
-        const getFileContent = () => {
-          const pathway = path.join(__dirname, `/productData/productData${i}.csv`);
-          console.log('right here i am');
-          console.log(i);
-          fs.readFile(pathway, (err, fileData) => {
-            if (err) {
-              throw 'error writing product data into file';
-            }
-            console.log('here');
-            files = JSON.parse(fileData);
-            knex('products').insert(files);
-          });
-        };
-        // for (let n = 1; n < 11; n++) {
-
-        // return;
-        // }
-        writeFileContent(sampleProducts, getFileContent);
-      }
-      return;
-
-      // return knex('products').insert(sampleProducts);
+      const data = handleProductCSV();
+      return knex('products').insert(data);
     })
     .then(() => {
-      return knex('products')
-        .pluck('itemId')
-        .then(itemIds => {
-          console.log('here are the product ids---');
-          console.log(itemIds);
-          for (let i = 1; i < 31; i++) {
-            var variants = [];
-            for (let i = 0; i < itemIds.length; i++) {
-              let itemId = itemIds[i];
-              let variantList = generateFakeVariants(itemId);
-              variants.push(variantList);
-            }
-            console.log('final variant list');
-            console.log(variants);
-            var flattenVariantList = [].concat.apply([], variants);
-            const productInJSONFormat = JSON.stringify(flattenVariantList);
-            const pathway = path.join(__dirname, `/variantData/variantData${i}.csv`);
-            fs.writeFile(pathway, productInJSONFormat, err => {
-              if (err) {
-                throw 'error writing product data into file';
-              } else {
-                console.log('successfully wrote variant csv file');
-              }
-            });
-          }
-
-          for (let n = 1; n < 31; n++) {
-            const pathway = path.join(__dirname, `/variantData/variantData${n}.csv`);
-            fs.readFile(pathway, (err, fileData) => {
-              if (err) {
-                throw 'error writing product data into file';
-              } else {
-                files = JSON.parse(fileData);
-                knex('variants').insert(files);
-              }
-            });
-          }
-          return;
-
-          // return knex('variants').insert(flattenVariantList);
-        });
+      const data = handleVariantCSV();
+      return knex('variants').insert(data);
+    })
+    .catch(err => {
+      console.log('Error', err);
     });
 };
