c949880a514e3f2cc487f1c2a3fe199ff0fb7367
Dawn Saquin <dawnsaq@gmail.com>
2019-03-04 11:36:01 -0800

    removed csvs to gitignore

--- db/example.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE dbname;
-USE dbname;
-CREATE TABLE tablename ( id smallint unsigned not null auto_increment, name varchar(20) not null, constraint pk_example primary key (id) );
-INSERT INTO tablename ( id, name ) VALUES ( null, 'Sample data' );
