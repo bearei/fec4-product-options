69099f301c281136416c3a31b91140b6f6ba4c9f
Dawn <dawnsaq@gmail.com>
2019-03-14 14:43:00 -0700

    endpoints for loader changed

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
-app.use('/:itemId', express.static(__dirname + '/../public'));
+// app.use('/:itemId', express.static(__dirname + '/../public'));
 app.use('/', express.static(path.join(__dirname, '../loaderio')));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
