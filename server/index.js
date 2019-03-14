105ff290ecf1a7164333133a53cd4ff8411832ab
Dawn <dawnsaq@gmail.com>
2019-03-14 14:20:57 -0700

    configuring loaderio

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
-app.use('/:itemId', express.static(__dirname + '/../public'));
+app.use('/', express.static(__dirname + '/../loaderio'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
