f92d29bbba0b7a9883cfcb48a24d335b9049c8de
Dawn <dawnsaq@gmail.com>
2019-03-13 18:46:17 -0700

    proxy service enabled

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-require('newrelic');
+// require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 // const cors = require('cors');
 
-const {
-  getProduct,
-  createProduct,
-  updateProduct,
-  deleteProduct,
-  getVariants
-} = require('../db/index');
+// const {
+//   getProduct,
+//   createProduct,
+//   updateProduct,
+//   deleteProduct,
+//   getVariants
+// } = require('../db/index');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -22,62 +22,78 @@ app.use('/:itemId', express.static(__dirname + '/../public'));
 /// for deployment to EC2 ////////
 /// setup as a proxy for db //////
 //////////////////////////////////
-// const proxy = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
 
-// app.use(
-//   '/products/:itemId',
-//   proxy({
-//     target: 'http://3.83.29.79:3002',
-//     changeOrigin: true
-//   })
-// );
+app.use(
+  '/products/:itemId',
+  proxy({
+    target: 'http://3.83.29.79:3002',
+    changeOrigin: true
+  })
+);
 
-// app.use(
-//   '/variants/:itemId',
-//   proxy({
-//     target: 'http://3.83.29.79:3002',
-//     changeOrigin: true
-//   })
-// );
+app.use(
+  '/variants/:itemId',
+  proxy({
+    target: 'http://3.83.29.79:3002',
+    changeOrigin: true
+  })
+);
 
-// app.use(
-//   '/products/',
-//   proxy({
-//     target: 'http://3.83.29.79:3002',
-//     changeOrigin: true
-//   })
-// );
+app.use(
+  '/products/',
+  proxy({
+    target: 'http://3.83.29.79:3002',
+    changeOrigin: true
+  })
+);
 
 //////////////////////////////////
 //////////////////////////////////
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   host: '3.83.29.79',
+//   user: 'root',
+//   password: '',
+//   database: 'product_options'
+// });
 
-app.get('/products/:itemId', function gettingProducts(req, res) {
-  getProduct(req.params.itemId)
-    .then(product => {
-      res.status(200).send(product);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
 
-app.get('/variants/:itemId', function gettingVariants(req, res) {
-  const itemId = req.params.itemId;
-  getVariants(itemId)
-    .then(variants => {
-      res.status(200).send(variants);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
+//   console.log('connected as id ' + connection.threadId);
+// });
 
-app.post('/products/', function postingProduct(req, res) {
-  const product = req.body;
-  createProduct(product).then(() => {
-    res.status(200).end();
-  });
-});
+// app.get('/products/:itemId', function gettingProducts(req, res) {
+//   getProduct(req.params.itemId)
+//     .then(product => {
+//       res.status(200).send(product);
+//     })
+//     .catch(err => {
+//       res.status(500).send(err);
+//     });
+// });
+
+// app.get('/variants/:itemId', function gettingVariants(req, res) {
+//   const itemId = req.params.itemId;
+//   getVariants(itemId)
+//     .then(variants => {
+//       res.status(200).send(variants);
+//     })
+//     .catch(err => {
+//       res.status(500).send(err);
+//     });
+// });
+
+// app.post('/products/', function postingProduct(req, res) {
+//   const product = req.body;
+//   createProduct(product).then(() => {
+//     res.status(200).end();
+//   });
+// });
 
 // app.put('/products/:itemId', (req, res) => {
 //   const updatedItem = req.body;
