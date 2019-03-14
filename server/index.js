1662b66f661c442801bc691a6f679d345a1eaf74
Dawn <dawnsaq@gmail.com>
2019-03-14 16:03:01 -0700

    setting up redis

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 // const cors = require('cors');
 const path = require('path');
 
+var redis = require('redis'),
+  client = redis.createClient();
+
 const {
   getProduct,
   createProduct,
@@ -86,15 +89,41 @@ console.log('here in server!!!!');
 //   })
 // );
 
-app.get('/products/:itemId', function gettingProducts(req, res) {
-  getProduct(req.params.itemId)
-    .then(product => {
-      res.status(200).send(product);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
+///////////////////////////////////
+/////// Trying out Redis //////////
+///////////////////////////////////
+
+client.on('error', function(err) {
+  console.log('Error ' + err);
 });
+
+const findProductCache = (req, res) => {
+  client.get(req.params.id, (err, data) => {
+    if (data) {
+      res.send(data);
+    } else {
+      getProduct(req.params.id).then(product => {
+        client.setex(req.params.id, 120, JSON.stringify(product));
+        res.send(product);
+      });
+    }
+  });
+};
+app.get('/products/:itemId', findProductCache);
+
+///////////////////////////////////
+///////////////////////////////////
+///////////////////////////////////
+
+// app.get('/products/:itemId', function gettingProducts(req, res) {
+//   getProduct(req.params.itemId)
+//     .then(product => {
+//       res.status(200).send(product);
+//     })
+//     .catch(err => {
+//       res.status(500).send(err);
+//     });
+// });
 
 app.get('/variants/:itemId', function gettingVariants(req, res) {
   const itemId = req.params.itemId;
